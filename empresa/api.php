3e204ad2f66aa011bd6748e27a2cd49bd13a0c3a
Alejandro Gonzaga Leles <alejandrogonzaguinha@gmail.com>
2024-08-05 13:45:04 -0300

    Add files via upload

---
+++ empresa/api.php
@@ -0,0 +1,78 @@
+<?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $codigoSerie = $_GET['codigoSerie'];
+
+        if($codigoSerie == '11 - SELIC'){
+            $codigoSerie = '11';
+        }elseif($codigoSerie == '4175 - IGPM'){
+            $codigoSerie = '4175';
+        }elseif($codigoSerie == '11426 - IPCA'){
+            $codigoSerie = '11426';
+        }
+
+        $dataInicialNormal = $_GET['dataInicial'];
+        $dataFinalNormal = $_GET['dataFinal'];
+
+     $dataInicial = date('d/m/Y', strtotime($dataInicialNormal));
+        $dataFinal = date('d/m/Y', strtotime($dataFinalNormal));
+
+echo $codigoSerie;
+echo "<br>";
+        $valor = $_GET['valor'];
+    }
+
+    //$api_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.$codigoSerie/dados?formato=json&dataInicial=$dataInicial&dataFinal=$dataFinal";
+    $api_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.$codigoSerie/dados?formato=json&dataInicial=$dataInicial&dataFinal=$dataFinal";
+    
+
+    // Define a chave de acesso da API, se necessário
+    // $api_key = "sua_chave_de_acesso";
+
+    // Configura as opções de contexto para a solicitação
+    $options = [
+        'http' => [
+            'method' => 'GET',
+            'header' => 'User-Agent: PHP',
+            // Adicione a chave de acesso, se necessário
+            // 'header' => "Authorization: Bearer $api_key",
+        ],
+    ];
+
+    $context = stream_context_create($options);
+
+    // Faz a solicitação à API
+    $response = file_get_contents($api_url, false, $context);
+
+    // Verifica se a solicitação foi bem-sucedida
+    if($response !== false){
+        // Decodifica a resposta JSON para um array associativo
+        $data = json_decode($response, true);
+
+        // Verifica se a decodificação foi bem-sucedida
+        if($data !== null){
+            // A resposta agora está no formato de um array associativo
+            // Você pode acessar os dados conforme necessário
+            $indicePercentual = 0;
+            foreach ($data as $entry){
+                $indicePercentual = $indicePercentual + $entry['valor'];
+                // echo "Data: " . $entry['data'] . "<br>";
+                // echo "Valor: " . $entry['valor'] . "<br>";
+                // echo "Sub total: " . $total . "<br>";
+                // echo "<br>";
+            }
+            
+            $indicePercentual = ($indicePercentual / 100) + 1;
+            
+            echo "Índice: " . $indicePercentual . "<br>";
+
+            $valorAtualizado = $indicePercentual * $valor;
+
+            echo "Valor contrato atualizado: " . $valorAtualizado;
+        }else{
+            echo "Erro na decodificação JSON.";
+        }
+    }else{
+        echo "Erro na solicitação à API.";
+    }
+?>
---
+++ empresa/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div>
+            <h1>Atualizador de Contratos</h1>
+        </div>
+
+        <div>
+            <form action="api.php" method="GET">
+                <div>
+                    <label for="codigoSerie">Selecione um índice</label>
+                    <select name="codigoSerie">
+                        <option>11 - SELIC</option>
+                        <option>4175 - IGPM</option>
+                        <option>11426 - IPCA</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="dataInicial">Data início contrato:</label>
+                    <input type="date" id="dataInicial" name="dataInicial">
+                </div>
+
+                <div>
+                    <label for="dataFinal">Data fim contrato:</label>
+                    <input type="date" id="dataFinal" name="dataFinal">
+                </div>
+
+                <div>
+                    <label for="valor">Valor do contrato:</label>
+                    <input type="text" id="valor" name="valor">
+                </div>
+
+                <div>
+                    <button type="submit">Calcular</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
+
